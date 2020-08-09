05ed2909e7ccbc0a1e11128f6a85cfd7b9d0adfe
Anthony Price <antprice07@gmail.com>
2020-08-09 15:52:56 -0400

    armor repo

---
+++ arivana-web/src/main/java/com/rdsq/arivana/db/ArmorRepository.java
@@ -0,0 +1,9 @@
+package com.rdsq.arivana.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rdsq.arivana.business.Armor;
+
+public interface ArmorRepository extends JpaRepository<Armor, Integer> {
+
+}
